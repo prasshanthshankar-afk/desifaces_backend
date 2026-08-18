6f374cc5ff4d7bb8ef5afa573abeecc3a66c29e6
prasshanthshankar-afk <prasshanth.shankar@gmail.com>
2026-08-18 10:19:47 -0400

    v3 multiperson: export story generation bridge

--- services/shared/python/desifaces_shared/v3/__init__.py
+++ services/shared/python/desifaces_shared/v3/__init__.py
@@ -7,11 +7,14 @@ storytelling without depending on FastAPI or any provider SDK.
 from .media_store import CanonicalMediaStore, MediaOwnershipError
 from .generation_store import CanonicalGenerationStore, InvalidJobTransition
 from .story_store import CanonicalStoryStore, StoryGraphNotFound, StoryOwnershipError
+from .story_generation import CanonicalStoryGenerationStore, StoryGenerationPersistenceResult
 
 __all__ = [
     "CanonicalMediaStore",
     "CanonicalGenerationStore",
     "CanonicalStoryStore",
+    "CanonicalStoryGenerationStore",
+    "StoryGenerationPersistenceResult",
     "MediaOwnershipError",
     "InvalidJobTransition",
     "StoryGraphNotFound",
